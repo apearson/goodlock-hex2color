d39440e9c283d554194715aece46044cbc1d47d4
Andrew Pearson <andrew@andrewpe.com>
2016-10-06 22:49:46 -0500

    Fixing screen density detection

--- hex2goodlock.py
+++ hex2goodlock.py
@@ -4,9 +4,10 @@ import colorsys
 
 # Decided to use built in adb binary or now
 useSystemADB = True
-hasADB = os.popen("which adb").readline().strip()
-if sys.platform == "darwin" and not hasADB:
-    useSystemADB = False
+if sys.platform == "darwin":
+    hasADB = os.popen("which adb").readline().strip()
+    if not hasADB:
+        useSystemADB = False
 
 # Setting start of command
 commandStart = "adb "
@@ -19,32 +20,36 @@ inputHex = sys.argv[1]
 print("Input Hex: " + inputHex)
 
 # Converting from hex string to decimal values
+print("Parsing hex to rgb")
 rgb = (int(inputHex[1:3],16), int(inputHex[3:5], 16), int(inputHex[5:7], 16))
 
 # Converting to correct color space
+print("Converting rgb into hsv space")
 hsv = colorsys.rgb_to_hsv(rgb[0]/255, rgb[1]/255, rgb[2]/255)
 
-print("HSV")
-print(hsv)
-
 # Figuring out if in standard display mode or condensed
-displayMode = len(os.popen("adb shell wm density").readlines());
+print("Querying device for screen density")
+densityOutput = os.popen("adb shell wm density").read()
 
 # Screen Settings
-if displayMode == 1:
-    print('Display Mode: Standard')
+print("Parsing screen density")
+if "Override density: 560" in densityOutput:
+    print("Display Mode: Condensed")
+    HueAreaStart = (826, 924)  # X, Y
+    HueAreaSize = (69 ,466) # Width, Height
+    SVAreaStart = (249,924) # X, Y
+    SVAreaSize = (562,466) # Width, Height
+elif "Physical density: 640" in densityOutput:
+    print("Display Mode: Standard")
     HueAreaStart = (852, 871)  # X, Y
     HueAreaSize = (19 ,534) # Width, Height
     SVAreaStart = (182,871) # X, Y
     SVAreaSize = (655,534) # Width, Height
 else:
-    print('Display Mode: Condensed')
-    HueAreaStart = (826, 924)  # X, Y
-    HueAreaSize = (69 ,466) # Width, Height
-    SVAreaStart = (249,924) # X, Y
-    SVAreaSize = (562,466) # Width, Height
+    sys.exit("Display Density Not Supported!")
 
 # Finding Location on screen
+print('Finding correct screen locations')
 
 # Hue Bar on right side
 hueLocationMiddle  = round((HueAreaStart[0] + (HueAreaStart[0] + HueAreaSize[0])) / 2)
@@ -60,8 +65,10 @@ print("Hue Screen Location: %sx%s" % (hueLocationMiddle, hueLocation))
 print("Saturation Screen Location: %spx" % saturationLocation)
 print("Value Screen Location: %spx" % valueLocation)
 
-
+print("Creating command for device")
 command = commandStart + "shell input tap %s %s && adb shell input tap %s %s" % (hueLocationMiddle, hueLocation, saturationLocation, valueLocation)
 
 print(command)
+
+print("Running command on device")
 os.system(command)
